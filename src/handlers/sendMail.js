2873aaa37520dbe1bbb704bcdf7032b520ecd5ea
hsimao <e087754958@gmail.com>
2021-04-10 14:49:10 +0800

    feat: add aws send email service

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,6 +1,32 @@
+import AWS from "aws-sdk";
+
+const ses = new AWS.SES({ region: "eu-west-1" });
+
 async function sendMiail(event, context) {
-  console.log(event);
-  return event;
+  const params = {
+    Source: "e087754958@gmail.com",
+    Destination: {
+      ToAddresses: ["e087754958@gmail.com"]
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "Hello from Mars"
+        }
+      },
+      Subject: {
+        Data: "Test Mail"
+      }
+    }
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMiail;
